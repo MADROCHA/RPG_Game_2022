c43817ee90befd6ad6ff49d4a5c019e953d11aae
MADROCHA <madlarocha@gmail.com>
2022-10-05 15:53:44 +0200

    rpgGame/spriteAnimation/foreGroundObjects

---
+++ classes.js
@@ -0,0 +1,80 @@
+class Sprite {
+    constructor({
+        position,
+        velocity,
+        image,
+        frames = {
+            max:1,
+        }
+    }){
+        this.position = position
+        this.image = image
+        this.frames = {...frames, val:0, elapsed:0} 
+
+        this.image.onload = ()=> {
+            this.width = this.image.width / frames.max
+            this.height = this.image.height 
+        }
+        this.moving = false
+    }
+    draw(){
+        /* c.drawImage(
+            this.image, 
+            this.position.x, 
+            this.position.y, 
+            
+        ) */
+        c.drawImage(
+            this.image,
+            this.frames.val * this.width,
+            0,
+            this.image.width / this.frames.max,
+            this.image.height,
+            
+            this.position.x,
+            this.position.y,
+
+            this.image.width / this.frames.max,
+            this.image.height,
+            )
+            if(!this.moving) return
+                if(this.frames.max >1){
+                    this.frames.elapsed++
+                }
+                if(this.frames.elapsed % 10 === 0)
+                if(this.frames.val < this.frames.max -1){
+                    this.frames.val++
+                } else this.frames.val = 0
+            
+    }
+    update(){
+
+    }
+}
+
+class Boundary {
+    static width = 48
+    static height = 48
+    constructor({
+        position,
+    }){
+        this.position = position
+        this.width = 48 
+        this.height = 48
+    }
+    draw(){
+        c.fillStyle = ' rgb(0, 0, 255, 0.2)'
+        c.fillRect(
+            this.position.x,
+            this.position.y,
+            this.width,
+            this.height,
+        )
+    }
+}
+const boundaries = []
+const offset = {
+    x:-735,
+    y:-650
+} 
+
--- index.js
+++ index.js
@@ -9,98 +9,31 @@ for (let i = 0; i< collisions.length; i+=70){
 }
 //console.log(collisionsMap)
 
-class Boundary {
-    static width = 48
-    static height = 48
-    constructor({
-        position,
-    }){
-        this.position = position
-        this.width = 48 
-        this.height = 48
-    }
-    draw(){
-        c.fillStyle = ' rgb(0, 0, 255, 0.2)'
-        c.fillRect(
-            this.position.x,
-            this.position.y,
-            this.width,
-            this.height,
-        )
-    }
-}
-const boundaries = []
-const offset = {
-    x:-735,
-    y:-650
-} 
-    collisionsMap.forEach((row, i) =>{
-        row.forEach((symbol, j) => {
-            if (symbol === 1025)
-            //console.log(symbol)
-            boundaries.push(
-                new Boundary({
-                    position:{
-                        x:j * Boundary.width +offset.x,
-                        y:i * Boundary.height +offset.y
-                    }
-            })
-        )
-    })
-})
 
 c.fillStyle = 'azure'
 c.fillRect(0,0,canvas.width,canvas.height)
 
 const image = new Image()
+image.src = './img/maps/PelletTown1.png'
+const foregroundImage = new Image()
+foregroundImage.src = './img/maps/PelletTownForeground.png'
 const playerImage = new Image()
 playerImage.src = './img/player/playerDown.png'
-image.src = './img/maps/PelletTown1.png'
 
-class Sprite {
-    constructor({
-        position,
-        velocity,
-        image,
-        frames = {
-            max:1,
-        }
-    }){
-        this.position = position
-        this.image = image
-        this.frames = frames 
-
-        this.image.onload = ()=> {
-            this.width = this.image.width / frames.max
-            this.height = this.image.height 
-        }
-
-    }
-    draw(){
-        /* c.drawImage(
-            this.image, 
-            this.position.x, 
-            this.position.y, 
-            
-        ) */
-        c.drawImage(
-            this.image,
-            0,
-            0,
-            this.image.width / this.frames.max,
-            this.image.height,
-            
-            this.position.x,
-            this.position.y,
-
-            this.image.width / this.frames.max,
-            this.image.height,
-            )
-    }
-    update(){
-
-    }
-}
+collisionsMap.forEach((row, i) =>{
+    row.forEach((symbol, j) => {
+        if (symbol === 1025)
+        //console.log(symbol)
+        boundaries.push(
+            new Boundary({
+                position:{
+                    x:j * Boundary.width +offset.x,
+                    y:i * Boundary.height +offset.y
+                }
+        })
+    )
+})
+})
 const player = new Sprite({
     position: {
         x: canvas.width * 0.5 -192 *0.25 * 0.5,
@@ -121,6 +54,13 @@ const background = new Sprite({
     },
     image: image
 })
+const foreground = new Sprite({
+    position:{
+        x: offset.x,
+        y: offset.y,
+    },
+    image: foregroundImage
+})
 
 const keys = {
     w:{
@@ -138,7 +78,7 @@ const keys = {
 }
     
 
-    const movables = [background, ...boundaries]
+    const movables = [background, ...boundaries, foreground]
     function rectCollision({rect1,rect2}){
         return (
             rect1.position.x + rect1.width >= rect2.position.x
@@ -149,19 +89,21 @@ const keys = {
     }
     function animate(){
         window.requestAnimationFrame(animate)
-        background.draw()
         
-        player.draw()
-        //c.drawImage(image1, -735,-600)
+        background.draw()
         boundaries.forEach(boundary => {
             boundary.draw()
-
         }) 
+        player.draw()
+        foreground.draw()
+        //c.drawImage(image1, -735,-600)
+
 
             //if (playerImage.position.x + playerImage.width)
         //collisions
         //controls
         let moving = true
+        player.moving = false
         if (keys.w.pressed
             && lastKey === 'w'){
                 for (let i = 0; i < boundaries.length; i++){
@@ -209,6 +151,7 @@ const keys = {
                 })
         } else if(keys.s.pressed
             && lastKey === 's'){
+                player.moving = true
                 for (let i = 0; i < boundaries.length; i++){
                     const boundary = boundaries[i]
                     if(
